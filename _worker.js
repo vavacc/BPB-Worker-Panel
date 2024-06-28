596f1641ebe35e017d924ce1e0122eddcbcf789e
vava <58034771+vavacc@users.noreply.github.com>
2024-06-28 16:59:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,12 +7,24 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '50bd0396-8225-4422-86df-65fb0b087969';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
-
+//const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= [
+	"HK.vavaee.tk",
+	"US.vavaee.tk",
+	"KR.vavaee.tk",
+	"JP.vavaee.tk",
+	"SG.vavaee.tk",
+	"GB.vavaee.tk",
+	"DE.vavaee.tk",
+	"NL.vavaee.tk",
+	"CA.vavaee.tk",
+	"FR.vavaee.tk",
+	"TW.vavaee.tk",
+];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 
